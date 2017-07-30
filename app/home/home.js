4ec2d3d8ca08bb6626b884a015ab3c88bb71d9f5
TK-20466 <sarahbourt@gmail.com>
2017-07-29 20:26:43 -0400

    Updated some events
    Updated some events

--- app/home/home.js
+++ app/home/home.js
@@ -2,6 +2,9 @@ angular.module("ctgapp")
 .controller("homePageController", ["$scope", "$timeout", function($scope, $timeout) {
    $scope.images = [];
    $scope.images.push(
+      addImage("emt.jpg", "Stratford EMS Open House")
+   );
+   $scope.images.push(
       addImage("prospect1.jpg", "Prospect Theatre Rogue One Premiere")
    );
    $scope.images.push(
